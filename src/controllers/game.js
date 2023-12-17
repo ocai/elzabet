692e5f679a8684a312a055c1361756a862b5f1f1
Dillon Li <green.koopa667@gmail.com>
2023-12-17 02:35:59 -0500

    create and get functions

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,29 +1,80 @@
 'user strict'
 
+const { dbConn } = require('../../config/db.config');
+
 function create(info) {
-    return 0;
+    let gameInfo = Object.assign({
+        'status': "in_progress",
+        'result': null,
+        'createdAt': new Date(),
+        'updatedAt': new Date(),
+    }, info)
+    try {
+        const game = dbConn('games')
+            .insert(gameInfo)
+            .into('games')
+            .then((game) => {
+                return {id: game[0], ...gameInfo}
+            });
+        return game;
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 }
 
-function get(id) {
-    return 0;
+/**
+ * 
+ * @param {*} info {}
+ * info is an object containing any combination of queryable fields 
+ * @returns 
+ * a single game
+ */
+
+function get(info) {
+    let acceptedFields = [
+        'id',
+        'playerId',
+        'riotGameId',
+        'riotMatchId',
+        'status',
+        'result',
+        // probably won't use these 2, but they're here
+        'createdAt',
+        'updatedAt'
+    ]
+    try {
+        if ((Object.keys(info).length == 0) || (Object.keys(info).length > acceptedFields.length))
+            throw Error("Invalid Object");
+        const game = dbConn('games')
+            .first(['*'])
+            .from('games')
+            .where((builder) => {
+                for (const [key, value] of Object.entries(info)) {
+                    if (acceptedFields.includes(key)) {
+                        builder.where(key, value);
+                    } else {
+                        throw Error("Invalid Object")
+                    }                   
+                }
+            })
+        return game;
+    } catch(error) {
+        console.log("Something went wrong", error);
+    }
 }
 
-function getInProgress(id) {
-    return {};
-}
-
-function update(id, body) {
-    return 0;
-}
-
-function remove(id) {
-    return 0;
+/**
+ * 
+ * @param {*} playerId 
+ * @param {*} result (win | loss)
+ * @returns 
+ */
+function update(playerId, result) { 
+    return 0
 }
 
 module.exports = {
     create,
     get,
-    getInProgress,
-    update,
-    remove
+    update
 };
