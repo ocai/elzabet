bc096c2799fdd8be20e06b960ee619aaac8e9af4
Dillon Li <green.koopa667@gmail.com>
2023-12-17 19:55:12 -0500

    Game module (#12)
    * created tests for game module
    
    * create and get functions
    
    * corrected game update tests from object input to individual params
    
    * update function
    
    ---------
    
    Co-authored-by: Oliver Cai <oliver@olivercai.com>

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,29 +1,111 @@
 'user strict'
 
+const { dbConn } = require('../../config/db.config');
+
 function create(info) {
-    return 0;
+    let gameInfo = Object.assign({
+        'status': "in_progress",
+        'result': null,
+        'createdAt': new Date(),
+        'updatedAt': new Date(),
+    }, info)
+    try {
+        const game = dbConn('games')
+            .insert(gameInfo)
+            .into('games')
+            .then((game) => {
+                return {id: game[0], ...gameInfo}
+            });
+        return game;
+    } catch (error) {
+        console.error(error);
+        throw(error);
+    }
 }
 
-function get(id) {
-    return 0;
+/**
+ * 
+ * @param {*} info {}
+ * info is an object containing any combination of queryable fields 
+ * @returns 
+ * a single game
+ */
+
+function get(info) {
+    const acceptedFields = [
+        'id',
+        'playerId',
+        'riotGameId',
+        'riotMatchId',
+        'status',
+        'result',
+        // probably won't use these 2, but they're here
+        'createdAt',
+        'updatedAt'
+    ]
+    try {
+        if ((Object.keys(info).length == 0) || (Object.keys(info).length > acceptedFields.length))
+            throw Error("Invalid Object");
+        const game = dbConn('games')
+            .first(['*'])
+            .from('games')
+            .where((builder) => {
+                for (const [key, value] of Object.entries(info)) {
+                    if (acceptedFields.includes(key)) {
+                        builder.where(key, value);
+                    } else {
+                        console.log(`Invalid key: ${key}`);
+                        throw Error("Invalid Object")
+                    }                   
+                }
+            })
+        return game;
+    } catch(error) {
+        console.log("Something went wrong", error);
+        throw(error);
+    }
 }
 
-function getInProgress(id) {
-    return {};
-}
-
-function update(id, body) {
-    return 0;
-}
-
-function remove(id) {
-    return 0;
+/**
+ * 
+ * @param {*} id: id of the game 
+ * @param {*} result (win | loss)
+ * @returns 
+ */
+function update(id, result) { 
+    const acceptedResults = ['win', 'loss'];
+    try {
+        if (!acceptedResults.includes(result)) {
+            console.log('in here')
+            throw Error(`Invalid result: ${result}`);
+        } else {
+            const game = dbConn('games')
+                .where('id', '=', id).andWhere('status', '=', 'in_progress')
+                .update({
+                    'result': result,
+                    'status': 'final',
+                    'updatedAt': new Date()
+                })
+                .then((res) => {
+                    if (!res) {
+                        console.log(`Invalid Query for game with id ${id}`)
+                        throw Error(`Invalid Query for game with id ${id}`);
+                    }
+                    const updatedGame = get({'id': id}).then((response) => {
+                        return response;
+                    });
+                    return updatedGame;
+                })
+            return game;
+        }
+    } catch (error) {
+        console.log("Something went wrong: ", error)
+        throw(error);
+    }
 }
 
 module.exports = {
     create,
     get,
-    getInProgress,
-    update,
-    remove
+    update
 };
---
+++ tests/controllers/game.test.js
@@ -0,0 +1,203 @@
+'use strict'
+
+const game = require('../../src/controllers/game');
+const { dbConn } = require('../../config/db.config');
+const f = require('../support/factory');
+
+describe('Game: get', () => {
+    test('returns a game correctly using the game id', async () => {
+        const [gameId] = await dbConn('games').insert(f.game(
+            {
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'in_progress',
+                'result': 'loss'
+            }
+        ));
+        const res = await game.get({'id': gameId});
+        expect(res).toMatchObject({
+            'id': expect.any(Number),
+            'playerId': '1',
+            'riotGameId': '4846836721',
+            'riotMatchId': 'NA1_4846836721',
+            'status': 'in_progress',
+            'result': 'loss',
+            'createdAt': expect.any(Date),
+            'updatedAt': expect.any(Date)
+        })
+    });
+
+    test('returns a game correctly using the playerId AND riotGameId', async () => {
+        const [gameId] = await dbConn('games').insert(f.game(
+            {
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'in_progress',
+                'result': 'loss'
+            }
+        ));
+        const res = await game.get({'playerId': '1', 'riotGameId': '4846836721'});
+        expect(res).toMatchObject({
+            'id': expect.any(Number),
+            'playerId': '1',
+            'riotGameId': '4846836721',
+            'riotMatchId': 'NA1_4846836721',
+            'status': 'in_progress',
+            'result': 'loss',
+            'createdAt': expect.any(Date),
+            'updatedAt': expect.any(Date)
+        })
+    });
+
+    test('returns a game correctly using the playerId AND riotMatchId', async () => {
+        const [gameId] = await dbConn('games').insert(f.game(
+            {
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'in_progress',
+                'result': 'loss'
+            }
+        ));
+        const res = await game.get({'playerId': '1', 'riotMatchId': 'NA1_4846836721'});
+        expect(res).toMatchObject({
+            'id': expect.any(Number),
+            'playerId': '1',
+            'riotGameId': '4846836721',
+            'riotMatchId': 'NA1_4846836721',
+            'status': 'in_progress',
+            'result': 'loss',
+            'createdAt': expect.any(Date),
+            'updatedAt': expect.any(Date)
+        })
+    });
+
+    test('returns an error if using an invalid object', async () => {
+        expect(async () => await game.get({'gibberish': 'test'})).rejects.toThrow();
+    });
+});
+
+describe('Game: create', () => {
+    test('returns a game after creating an in-progress game', async () => {
+        const res = await game.create(
+            {
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'in_progress'
+            }
+        );
+        expect(res).toMatchObject({
+            'id': expect.any(Number),
+            'playerId': '1',
+            'riotGameId': '4846836721',
+            'riotMatchId': 'NA1_4846836721',
+            'status': 'in_progress',
+            'result': null,
+            'createdAt': expect.any(Date),
+            'updatedAt': expect.any(Date)
+        });
+    });
+
+    test('returns a game after creating a completed game', async () => {
+        const res = await game.create(
+            {
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'final',
+                'result': 'loss'
+            }
+        );
+        expect(res).toMatchObject({
+            'id': expect.any(Number),
+            'playerId': '1',
+            'riotGameId': '4846836721',
+            'riotMatchId': 'NA1_4846836721',
+            'status': 'final',
+            'result': 'loss',
+            'createdAt': expect.any(Date),
+            'updatedAt': expect.any(Date)
+        });
+    });
+
+    test('actually creates a game in the DB', async() => {
+        const res = await game.create(
+            {   
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'final',
+                'result': 'loss'
+            }
+        );
+        const newGame = await dbConn.first(['*']).from('games').where('id', '=', res['id']);
+        expect(newGame).toBeTruthy();
+    });
+});
+
+describe('Game: update', () => {
+    test('updates an in-progress game in the DB with a loss', async () => {
+        const [gameId] = await dbConn('games').insert(f.game(
+            {   
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'in_progress'
+            }
+        ));
+        const updatedGame = await game.update(gameId, 'loss');
+        expect(updatedGame).toMatchObject({
+            'playerId': '1',
+            'riotGameId': '4846836721',
+            'riotMatchId': 'NA1_4846836721',
+            'status': 'final',
+            'result': 'loss',
+            'createdAt': expect.any(Date),
+            'updatedAt': expect.any(Date)
+        })
+    });
+
+    test('updates an in-progress game in the DB with a win', async () => {
+        const [gameId] = await dbConn('games').insert(f.game(
+            {   
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'in_progress'
+            }
+        ));
+        const updatedGame = await game.update(gameId, 'win');
+        expect(updatedGame).toMatchObject({
+            'playerId': '1',
+            'riotGameId': '4846836721',
+            'riotMatchId': 'NA1_4846836721',
+            'status': 'final',
+            'result': 'win',
+            'createdAt': expect.any(Date),
+            'updatedAt': expect.any(Date)
+        })
+    });
+
+    test('return an error if there are no in-progress games', async () => {
+        expect(async () => await game.update('999', 'win')).rejects.toThrow();
+    });
+
+    test('return an error if passing in an invalid player', async () => {
+        expect(async () => await game.update('gibberish', 'win')).rejects.toThrow();
+    });
+
+    test('return an error if passing in an invalid result', async () => {
+        const [gameId] = await dbConn('games').insert(f.game(
+            {   
+                'playerId': '1',
+                'riotGameId': '4846836721',
+                'riotMatchId': 'NA1_4846836721',
+                'status': 'in_progress'
+            }
+        ));
+        expect(async () => await game.update(gameId, 'gibberish')).rejects.toThrow();
+    });
+});
